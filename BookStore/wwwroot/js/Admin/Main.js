40fdce8f9c7c74926504778f27a86b68b5b7ed5b
av4ermans@gmail.com <av4ermans@gmail.com>
2020-11-08 19:53:51 +0200

    Added angular. Create form for admin added books list to grid.

---
+++ BookStore/wwwroot/js/Admin/Main.js
@@ -0,0 +1,7 @@
+﻿var myApp = angular.module('myApp', []);
+
+myApp.controller('AdminController', ['$scope', async function AdminController($scope) {
+    $scope.books = await (await fetch('https://localhost:44310/api/Books/GetBookList')).json();
+       $scope.$apply();
+    }]);
+
