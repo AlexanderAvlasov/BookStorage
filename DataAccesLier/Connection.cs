c9d89ac3302899de0fee13cc3634feaf8424c567
av4ermans@gmail.com <av4ermans@gmail.com>
2020-11-15 10:06:22 +0200

    refactoring

--- DataAccesLier/Connection.cs
+++ DataAccesLier/Connection.cs
@@ -21,16 +21,6 @@ namespace DataAccesLier
                 Console.WriteLine($"Name :{sQLiteDataReader.GetString(1)} \nAuthor {sQLiteDataReader.GetString(2)} \nPages={sQLiteDataReader.GetDecimal(3)} "); ; ;
             }
             Console.WriteLine("Book store");
-
-            //using (SqlConnection con = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=C:\\USERS\\ALEXANDER\\SOURCE\\REPOS\\BOOKSTORAGE\\CONSOLEAPP1\\BIN\\DEBUG\\NETCOREAPP3.1\\BOOKSTORE.MDF;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False")) {
-            //    con.Open();
-            //    SqlCommand com = new SqlCommand("select * from books", con);
-            //    SqlDataReader reader = com.ExecuteReader();
-            //    while (reader.Read()) {
-            //        Console.WriteLine($"Name :{reader.GetString(0)} \nAuthor {reader.GetString(1)} \nPages={reader.GetString(2)} ");; ;
-            //    }
-
-            //}
         }
     }
 }
